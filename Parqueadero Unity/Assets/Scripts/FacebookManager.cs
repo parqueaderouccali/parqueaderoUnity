fcc3ffb450e5846a1d54d125bc114e12d7bf0563
parqueaderouccali <42127357+parqueaderouccali@users.noreply.github.com>
2018-08-20 17:43:53 -0500

    Auth Facebook
    Se realiza la configuracion de la autenticacion por Facebook

---
+++ Parqueadero Unity/Assets/Scripts/FacebookManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+using UnityEngine;
+
+public class FacebookManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Awake () {
+		
+		if(!FB.IsInitialized){
+			FB.Init();
+		
+	}else{
+
+		FB.ActivateApp();
+		
+		}
+	}
+
+   public void LogIn () {
+
+	   FB.LogInWithReadPermissions(callback:OnLogIn);
+		
+	}
+
+
+	private void OnLogIn(ILoginResult result) {
+
+		if(FB.IsLoggedIn){
+
+			AccessToken token = AccessToken.CurrentAccessToken;
+			print("usuario"+ token.UserId);
+		
+	}else{
+
+		print("Canceled");
+		
+		}
+
+	   
+		
+	}
+
+
+}
